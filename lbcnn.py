054d0952cd978856fa4c184d32459b79392bc978
Pedro Antonio <pedroantonio.fernandes.dev@gmail.com>
2023-11-20 12:13:37 -0300

    Inclusão de canais de saída

--- lbcnn.py
+++ lbcnn.py
@@ -28,12 +28,13 @@ class SubLayerLBC2D(LBCSubLayer):
 
 class LBC(tf.keras.layers.Layer):
     
-    def __init__(self, rank, anchor_weights, kernel_size, sub_layer1, sub_layer2, strides=1, padding='valid', activation='relu', kernel_initializer='glorot_uniform', kernel_regularizer=None, activity_regularizer=None, kernel_constraint=None, trainable=True, name=None, **kwargs):
+    def __init__(self, rank, anchor_weights, filters, kernel_size, sub_layer1, sub_layer2, strides=1, padding='valid', activation='relu', kernel_initializer='glorot_uniform', kernel_regularizer=None, activity_regularizer=None, kernel_constraint=None, trainable=True, name=None, **kwargs):
         super(LBC, self).__init__( trainable=trainable, name=name, activity_regularizer=regularizers.get(activity_regularizer), **kwargs)
         self.rank = rank
         self.kernel_size = kernel_size
         self.anchor_weights = tf.Variable(initial_value=anchor_weights, trainable=False)
-        self.filters = anchor_weights.shape[-1]
+        self.filters = filters
+        self.intermediary_filters = anchor_weights.shape[-1]
         self.strides = conv_utils.normalize_tuple(strides, rank, 'strides')
         self.padding = conv_utils.normalize_padding(padding)
         self.activation = activations.get(activation)
@@ -44,8 +45,8 @@ class LBC(tf.keras.layers.Layer):
         self.sublayer2 = sub_layer2
         
     def build(self, input_shape):
-        kernel_shape = self.kernel_size + (self.filters, 1)
-        self.kernel = self.add_weight( name='kernel', shape=kernel_shape, initializer=self.kernel_initializer, regularizer=self.kernel_regularizer, constraint=self.kernel_constraint, trainable=True, dtype=self.dtype)
+        kernel_shape = self.kernel_size + (self.intermediary_filters, self.filters)
+        self.kernel = self.add_weight(name='kernel', shape=kernel_shape, initializer=self.kernel_initializer, regularizer=self.kernel_regularizer, constraint=self.kernel_constraint, trainable=True, dtype=self.dtype)
 
     def call(self, inputs):
         y = self.sublayer1.calculate(inputs, self.anchor_weights)
@@ -55,11 +56,11 @@ class LBC(tf.keras.layers.Layer):
     
 class LBC2D(LBC):
     
-    def __init__(self, anchor_weights, strides=1, padding='valid', kernel_initializer='glorot_uniform', kernel_regularizer=None, activity_regularizer=None, kernel_constraint=None, **kwargs):
+    def __init__(self, anchor_weights, filters, strides=1, padding='valid', kernel_initializer='glorot_uniform', kernel_regularizer=None, activity_regularizer=None, kernel_constraint=None, **kwargs):
         sub_layer1 = SubLayerLBC2D(strides=strides, padding=padding.upper())
         sub_layer2 = SubLayerLBC2D(strides=1, padding='SAME')
         self.validate_anchor_weights(anchor_weights)
-        super(LBC2D, self).__init__(rank=2, anchor_weights=anchor_weights, kernel_size=(1,1), sub_layer1=sub_layer1, sub_layer2=sub_layer2, strides=strides, padding=padding, activation='relu', kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer, activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint, **kwargs)
+        super(LBC2D, self).__init__(rank=2, anchor_weights=anchor_weights, filters=filters, kernel_size=(1,1), sub_layer1=sub_layer1, sub_layer2=sub_layer2, strides=strides, padding=padding, activation='relu', kernel_initializer=kernel_initializer, kernel_regularizer=kernel_regularizer, activity_regularizer=activity_regularizer, kernel_constraint=kernel_constraint, **kwargs)
 
     def validate_anchor_weights(self, anchor_weights):
         if anchor_weights.max() > 1 or anchor_weights.max() < -1:
